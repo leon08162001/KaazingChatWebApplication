fec10353285537a79a8f55b313c071fdd252110f
moneysq_tw <leon08162001@gmail.com>
2023-06-25 07:15:56 +0800

    update

--- KaazingChatWebApplication/WebChat.aspx.designer.cs
+++ KaazingChatWebApplication/WebChat.aspx.designer.cs
@@ -1,17 +1,19 @@
 ﻿//------------------------------------------------------------------------------
-// <自動產生的>
+// <自動產生>
 //     這段程式碼是由工具產生的。
 //
 //     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
 //     程式碼，則會遺失變更。
-// </自動產生的>
+// </自動產生>
 //------------------------------------------------------------------------------
 
-namespace KaazingChatWebApplication {
-    
-    
-    public partial class WebChat {
-        
+namespace KaazingChatWebApplication
+{
+
+
+    public partial class WebChat
+    {
+
         /// <summary>
         /// form1 控制項。
         /// </summary>
@@ -20,7 +22,7 @@ namespace KaazingChatWebApplication {
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// chatRecords 控制項。
         /// </summary>
